2c645778df362d21406be34bc0c926a7169b05a6
Mark Scholes <markscholes85@gmail.com>
2017-01-21 11:28:40 +0000

    re-upload of link share
    Had to be reuploaded due to issue. Old version removed for github

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require("config.php"); ?>
+<?php require("classes/bootstrap.php");?>
+<?php require("classes/Controller.php");?>
+<?php require("classes/Model.php");?>
+
+<?php require("controllers/home.php");?>
+<?php require("controllers/shares.php");?>
+<?php require("controllers/users.php");?>
+
+<?php require("models/home.php");?>
+<?php require("models/share.php");?>
+<?php require("models/user.php");?>
+<?php
+
+$bootstrap = new Bootstrap($_GET);
+$controller = $bootstrap->createController();
+
+if($controller){
+	$controller->executeAction();
+}
